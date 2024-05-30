a28d0bbcd54869bb6076bc81da83749ac2db82b0
David Mbaimbai <dmbaimbai@csod.com>
2024-05-30 17:55:34 +0200

    E commerce app code solution

---
+++ services/notification-service/src/test/java/com/davymbaimbai/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davymbaimbai.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
